8f5902102159f71b736db5fc7c503b0e5974f105
ghulam-rasool-dev <ghulam.rasool.freelance@gmail.com>
2021-01-28 17:49:17 +0500

    Refactoring GenderIcon

--- lib/InputPage.dart
+++ lib/InputPage.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const borderButtonHeight = 60.0,
+    hexColorValue = Color(0xff1d1f33),
+    bottomButtonColor = Colors.pink;
 
 class InputPAge extends StatefulWidget {
   @override
@@ -21,36 +26,50 @@ class _InputPAgeState extends State<InputPAge> {
               child: Row(
                 children: [
                   Expanded(
-                    child: ReusableCard(Color(0xff1d1f33)),
+                    child: ReusableCard(
+                      color: hexColorValue,
+                      child: GenderIcon(
+                        icon: FontAwesomeIcons.mars,
+                        text: "MALE",
+                      ),
+                    ),
                   ),
                   Expanded(
-                    child: ReusableCard(Color(0xff1d1f33)),
+                    child: ReusableCard(
+                      color: hexColorValue,
+                      child: GenderIcon(
+                        icon: FontAwesomeIcons.venus,
+                        text: "FEMALE",
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
             Expanded(
               flex: 3,
-              child: ReusableCard(Color(0xff1d1f33)),
+              child: ReusableCard(color: hexColorValue),
             ),
             Expanded(
               flex: 2,
               child: Row(
                 children: [
                   Expanded(
-                    child: ReusableCard(Color(0xff1d1f33)),
+                    child: ReusableCard(color: hexColorValue),
                   ),
                   Expanded(
-                    child: ReusableCard(Color(0xff1d1f33)),
+                    child: ReusableCard(color: hexColorValue),
                   ),
                 ],
               ),
             ),
-            Expanded(
+            Padding(
+              padding: const EdgeInsets.all(5.0),
               child: FlatButton(
+                height: borderButtonHeight,
                 onPressed: () {},
                 child: Text('Press me'),
-                color: Colors.pink,
+                color: bottomButtonColor,
               ),
             )
           ],
@@ -58,14 +77,45 @@ class _InputPAgeState extends State<InputPAge> {
   }
 }
 
-class ReusableCard extends StatelessWidget {
-  ReusableCard(this.color);
-  final Color color;
+class GenderIcon extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  GenderIcon({@required this.icon, @required this.text});
 
   @override
   Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          color: Colors.white,
+          size: 80,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class ReusableCard extends StatelessWidget {
+  final Color color;
+  Widget child;
+  ReusableCard({@required this.color, this.child});
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(10),
+      child: child,
+      margin: EdgeInsets.all(5),
       decoration: BoxDecoration(
         color: color,
         borderRadius: BorderRadius.circular(10),
