29d7f99e29ea5974d5da96a711a93a366b43c11e
ghulam-rasool-dev <ghulam.rasool.freelance@gmail.com>
2021-01-28 13:45:18 +0500

    adding theme to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-
+  runApp(BMICalculator());
 }
 
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          primaryColor: Color(0xff090C20),
+          scaffoldBackgroundColor: Color(0xff090C20),
+          textTheme: TextTheme(
+            body1: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.grey.shade900),
+      home: InputPAge(),
+    );
+  }
+}
+
+class InputPAge extends StatefulWidget {
+  @override
+  _InputPAgeState createState() => _InputPAgeState();
+}
+
+class _InputPAgeState extends State<InputPAge> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BMI Calculator"),
+      ),
+      body: Center(
+        child: Text("Body Text"),
+      ),
+    );
+  }
+}
